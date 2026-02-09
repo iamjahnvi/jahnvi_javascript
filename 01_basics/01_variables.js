f25f2f4a6112dfe99b4f151b8d8060054ba78d77
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-02-09 19:15:30 +0530

    add 01_basics with variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+let accountState;
+
+// accountId = 2;  // not allowed
+accountEmail = "hc@gmail.com";
+accountPassword = "212121";
+accountCity = "Delhi";
+
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountEmail);
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block and functional scope
+*/
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; 
+// treat all js code as newer version
+
+// alert(3+3) 
+// we are using node.js , not browser.
+
+console.log(3+3) 
+
+// code should be readable.
+
+let fullName = "hitesh kumar"
+let age = 18
+let isLoggedIn = true 
+
+// PRIMITIVE DATATYPES
+
+// number => 2 to power 53
+// bigInt 
+// string => "" (enclosed in double quotes)
+// boolean => true/false
+// null => stand alone value (empty value)
+// undefined => value not defined/assigned
+// symbol => unique elements
+
+// NON-PRIMITIVE DATATYPES
+
+// typeof
+
+console.log(typeof "hitesh"); 
+// it will return string.
+
+console.log(typeof fullName);
+// it will also return string.
+
+console.log(typeof undefined);
+// it will return undefined.
+
+console.log(typeof null);
+// it will return object.
