314a65132385e6dcb5d280fd4c231b1990651bf9
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-02-10 00:37:10 +0530

    comparasion of datatypes

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,25 @@
+// COMPARASIONS OF TWO DIFFERENT DATATYPES
+
+// console.log("2" > 1)           // true - conversion of string to number.
+// console.log("02" > 1)          // true
+
+// console.log(0 == null)        // false
+// console.log(null < 0)         // false
+// console.log(null > 0)         // false
+// console.log(0 >= null)        // true
+// console.log(null <= 0)        // true
+
+// console.log(0 == undefined)        // false
+// console.log(undefined < 0)         // false
+// console.log(undefined > 0)         // false
+// console.log(0 >= undefined)        // false
+// console.log(undefined <= 0)        // false
+
+// === does strict checking , not only checkes value but also
+
+console.log("2" === 2)   // false , because alongwith value, also checks datatype.
+
+
+
+
+
