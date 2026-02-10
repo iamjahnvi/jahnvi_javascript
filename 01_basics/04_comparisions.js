9498eaf71c896aef0f48e9a15777d5468e4bb9bd
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-02-10 12:38:43 +0530

    comparisions in javascript

--- 01_basics/04_comparisions.js
+++ 01_basics/04_comparisions.js
@@ -1,7 +1,9 @@
 // COMPARASIONS OF TWO DIFFERENT DATATYPES
 
-// console.log("2" > 1)           // true - conversion of string to number.
-// console.log("02" > 1)          // true
+// console.log("2" > 1)          
+// true - conversion of string to number.
+
+// console.log("02" > 1)         // true
 
 // console.log(0 == null)        // false
 // console.log(null < 0)         // false
@@ -15,9 +17,10 @@
 // console.log(0 >= undefined)        // false
 // console.log(undefined <= 0)        // false
 
-// === does strict checking , not only checkes value but also
+// === does strict checking , not only checkes value but also datatype
 
-console.log("2" === 2)   // false , because alongwith value, also checks datatype.
+console.log("2" === 2)   
+// false , because alongwith value, also checks datatype.
 
 
 
