74ad473b7ca160d06b5f4953d63b69ea1af720a2
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-02-15 12:30:58 +0530

    if-else in js

---
+++ 04_controlFlow.js/one.js
@@ -0,0 +1,63 @@
+// if - else statement
+
+// const temp = 41
+
+// if(temp < 50){
+//     console.log("less than 50")
+// } else {
+//     console.log("not less than 50")
+// }
+// console.log("yeppie");
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+// }
+
+// const balance = 650
+
+// // if(balance > 500) console.log("richie rich") ;
+
+// if (balance < 500){
+//     console.log("very poor");
+// } else if(balance < 750){
+//     console.log("mediocre");
+// } else {
+//     console.log("richie rich");
+// }
+
+const userLoggedIn = true 
+const debitCard = true 
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(username && debitCard){
+    console.log("You are allowed to purchase courses")
+} 
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
