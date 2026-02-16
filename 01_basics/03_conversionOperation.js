9e8bf18a674bd48208138f18b967526b00edfe3d
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-02-16 11:14:29 +0530

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -81,6 +81,7 @@ case of 3 elements :
 // console.log(true+);  // throws errors
 
 // console.log(+"");    // 0
+// when we put "+" as prefix , then it doesn't change the value of whatever is written after it.
 
 // let num1 , num2 , num3
 // num1 = num2 = num3 = 2+2
