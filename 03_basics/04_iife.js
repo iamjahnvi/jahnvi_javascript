9a47a791037f0ef644019cb541d3c00786e08bae
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-03-03 23:03:03 +0530

    Comment out interview notes in IIFE example
    Commented out interview notes for clarity.

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -14,7 +14,7 @@ chai() ;
 // first parenthesis is where we are going to write the defination of function.
 // second parenthesis is put for execution purpose.
 
-//#interview - we introduced not only because we wanted function to get immediately invoked in the system but also because we want to prevent our block of code from the pollution due to declaration of global variables.
+// #interview - we introduced not only because we wanted function to get immediately invoked in the system but also because we want to prevent our block of code from the pollution due to declaration of global variables.
 
 
 // SIMPLE IIFE
@@ -25,11 +25,11 @@ chai() ;
 )('pixie') ;
 
 // first parenthesis - for packaging function in a block.
-// second parenthesis [ () => ]- to make arrow function.
+// second parenthesis [ () => ] - to make an arrow function.
 // pixie is the argument we passed we passed in the execution parenthesis
 // name is the parameter
 
-//#interview - also super important point to be remembered in case of IIFE is that , we have to compulsorily put semi-colon at the end of IIFE to tell the IIFE block of code that it is ending here.
+// #interview - also super important point to be remembered in case of IIFE is that , we have to compulsorily put semi-colon at the end of IIFE to tell the IIFE block of code that it is ending here.
 
 
 
